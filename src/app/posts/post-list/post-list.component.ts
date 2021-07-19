45fabc12d34a821a794f5839269b534cd11e791e
EChilin5 <edgar3ac@gmail.com>
2021-07-19 14:33:51 -0700

    added a post model

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from "@angular/core";
+import {Post} from '../post.model';
+
 
 @Component({
   selector:'app-post-list',
@@ -12,5 +14,5 @@ export class PostListComponent{
   //   {title:'Third post', content:"This is the third post"},
 
   // ];
-  @Input() posts=[];
+  @Input() posts:Post[]=[];
 }
