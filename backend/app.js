373016c57ff7b381dfed51c4930d15aeed443847
EChilin5 <edgar3ac@gmail.com>
2021-07-25 11:56:24 -0700

    able to add and delete post from database

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
 
 const app = express();
 
+// this is currently depecrated will need to update this portion with
+// latest way to update connection to  mongoose
+mongoose.connect("mongodb+srv://edgar:<password>@cluster0.iieuk.mongodb.net/node-angular?retryWrites=true&w=majority")
+  .then(()=>{
+    console.log("Connected to database");
+  })
+  .catch(()=>{
+    console.log("Connection failed");
+  })
+
 app.use(bodyParser.json());
+
+const Post = require('./models/post');
+
 
 
 app.use( (req, res, next) => {
@@ -16,22 +31,37 @@ app.use( (req, res, next) => {
 
 
 app.post("/api/posts", (req, res, next)=>{
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message:'Post added successfully'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  })
+  // add then in order to get the id back from the created post
+  post.save().then(createdPost =>{
+    res.status(201).json({
+        message:'Post added successfully',
+        postId: createdPost._id
+      });
   });
+
 });
 
 app.get("/api/posts",(req, res, next)=>{
-  const posts = [
-    {id: 'ujieosajt', title:'first server post', content:"hello from server"},
-    {id: 'gftrsgfdg', title:'second server post', content:"welcome from server"},
-    {id: 'gdfgfery', title:'third server post', content:"end from server"},
-  ];
-  res.status(200).json({
-    message:'Posts fetched succesfully',
-    posts:posts
+  Post.find()
+  .then(documents =>{
+    // this needs to be executed here in order to get all the post
+    // or else res will be called before the post are found
+    res.status(200).json({
+      message:'Posts fetched succesfully',
+      posts:documents
+    });
+  })
+});
+
+// id will be extracted by express which will take the id
+app.delete("/api/posts/:id", (req, res, next)=>{
+  Post.deleteOne({_id: req.params.id}).then(result =>{
+    console.log(result);
+    res.status(200).json({message:"Post Deleted"});
   });
 });
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Post} from './post.model';
-import {Subject} from 'rxjs'
+import {Subject} from 'rxjs';
+import {map} from 'rxjs/operators';
 // rxjs are obejcts that help pass data around
 import {HttpClient} from '@angular/common/http';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
@@ -18,9 +19,21 @@ export class PostsService{
     // [...this.post]
 
     // get will change it to js
-    this.http.get<{message:string, posts:Post[]}>('http://localhost:3000/api/posts')
-    .subscribe( (postData) =>{
-      this.posts = postData.posts;
+    this.http.get<{message:string, posts:any}>('http://localhost:3000/api/posts')
+    .pipe(map((postData)=>{
+      // this will be wrapped to an observable which will
+      // hold all the post
+      return postData.posts.map(post =>{
+       // this object will then be stored in an observable
+        return{
+          title:post.title,
+          content: post.content,
+          id: post._id
+        };
+      })
+    }))
+    .subscribe( (transformedPost) =>{
+      this.posts = transformedPost;
       this.postUpdated.next([...this.posts]);
     });
   }
@@ -33,15 +46,26 @@ export class PostsService{
   addPost(title:string, content:string){
     const post:Post = {id:null, title: title, content: content};
     this.http
-    .post<{ message:string }>("http://localhost:3000/api/posts",post)
-    .subscribe((responseData)=>{
-      console.log(responseData.message);
+    .post<{ message:string, postId: string }>("http://localhost:3000/api/posts",post)
 
+    .subscribe((responseData)=>{
+      const id = responseData.postId;
+      /// this will just updated the specific reference and not overide the entire object
+      post.id = id;
      // push will only post if subscribe is successful
       this.posts.push(post);
       //emiting
       this.postUpdated.next([...this.posts]);
     });
 
-  }
+   }
+
+   deletePost(postId:string){
+     this.http.delete("http://localhost:3000/api/posts/"+postId)
+     .subscribe(()=>{
+       const updatedPosts = this.posts.filter(post => post.id != postId);
+       this.posts = updatedPosts;
+       this.postUpdated.next([...this.posts]);
+     })
+   }
 }
